3b4daa4c85b60aaf589b533aa38d266149ba3d4b
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-11 18:15:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,15 +3,18 @@
 class Song
   attr_accessor @name , @artist , @genre
   @@count= 0
-  @@artists
-  @@genres
-  @@genre_count
-  @@artist_count
+  @@artists = []
+  @@genres = [] 
+  @@genre_count= {}
+  @@artist_count={}
 
   def initialize(name,artist,genre)
     @name,@artist , @genre = name , artist , genre
     @@count +=1
-
+    @@artists||=[]
+    @@artists << artist unless @@artists.include?(artist)
+    @@genres||=[]
+    @@genres <<
   end
 
 end
