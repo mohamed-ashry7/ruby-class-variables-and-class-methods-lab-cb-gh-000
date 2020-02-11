ce10a8642853e0fa407ef125764fd916cd231f97
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-11 18:13:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,14 +4,14 @@ class Song
   attr_accessor @name , @artist , @genre
   @@count= 0
   @@artists
-  @@genres 
+  @@genres
   @@genre_count
   @@artist_count
 
   def initialize(name,artist,genre)
-    @name,@artist , @genre = name , artist , genre  
+    @name,@artist , @genre = name , artist , genre
     @@count +=1
-    
+
   end
 
 end
