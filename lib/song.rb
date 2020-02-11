d09414d999723a861d80447898917a245d35ba0c
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-11 18:27:09 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+
+
+class Song
+  attr_accessor :name , :artist , :genre
+  @@count= 0
+  @@artists = []
+  @@genres = []
+  @@genre_count= {}
+  @@artist_count={}
+
+  def initialize(name,artist,genre)
+    @name , @artist , @genre = name , artist , genre
+    @@count +=1
+    @@artists << artist
+    @@genres << genre
+    @@genre_count[genre]||=0
+    @@genre_count[genre]+=1
+    @@artist_count[artist]||=0
+    @@artist_count[artist]+=1
+  end
+  def self.count
+    @@count
+  end
+  def self.artists
+    @@artists.uniq
+  end
+  def self.genres
+    @@genres.uniq
+  end
+  def self.genre_count
+    @@genre_count
+  end
+  def self.artist_count
+    @@artist_count
+  end
+end
